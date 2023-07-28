694ff7cbc67df3a0c519e8a663a388621b8eda6d
thejavidev <javidation.l@gmail.com>
2023-07-28 20:07:16 +0400

    2

---
+++ src/assets/index.js
@@ -0,0 +1,9 @@
+import line from './line.webp';
+import breadcump from './breadcump.webp';
+import cornlogo from './corn-logo.webp';
+import footer_logo from './footer_logo.png';
+import footer_bg from './footer_bg.webp';
+import moon from './moon.png';
+import soon from './son.png';
+
+export {line,breadcump,cornlogo,footer_bg,footer_logo,moon,soon}
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
+
+
 const About = () => {
   return (
     <div>
-      about 
+     
     </div>
   )
 }
