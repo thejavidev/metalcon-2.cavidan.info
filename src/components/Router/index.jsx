8d8563c14ef686f682bc1ff74935012e23238cd3
thejavidev <javidation.l@gmail.com>
2023-07-29 14:17:38 +0400

    redux unistall

--- src/components/Router/index.jsx
+++ src/components/Router/index.jsx
@@ -1,59 +1,66 @@
-import React, { useEffect } from 'react'
-import Home from '../../pages/Home'
-import About from '../../pages/About'
-import Services from '../../pages/Services'
-import Projects from '../../pages/Projects'
-import ProjectSingle from '../../pages/ProjectSingle'
-import Certificats from '../../pages/Certificats'
-import Contact from '../../pages/Contact'
-import { useRoutes } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
-import { loadposts } from '../store/posts'
+import React, { useEffect, useState } from "react";
+import Home from "../../pages/Home";
+import About from "../../pages/About";
+import Services from "../../pages/Services";
+import Projects from "../../pages/Projects";
+import ProjectSingle from "../../pages/ProjectSingle";
+import Certificats from "../../pages/Certificats";
+import Contact from "../../pages/Contact";
+import { useRoutes } from "react-router-dom";
+
+
 
 const Router = () => {
-
-    const dispatch =useDispatch();
-    const data =useSelector((state) =>state.list);
-
-    useEffect(()=>{
-        dispatch(loadposts())
-    },[dispatch])
-
+ 
+  const [data, setData] = useState([])
+   
+    const myurl =`${import.meta.env.VITE_API}fulldata`;
+  
+    useEffect(() => {
+      const getData = () => {
+        fetch(myurl)
+          .then(response => response.json())
+          .then(data => setData(data))
+          .catch(err => console.log(err))
+      }
+      getData()
+  
+    }, [])
     console.log(data)
-    
-    const minirouter =[
-        {
-            path:'/',
-            element: <Home data={data} />,
-            exact:true,
-        },
-        {
-            path:'/about',
-            element: <About />,
-        },
-        {
-            path:'/services',
-            element: <Services />, 
-        },
-        {
-            path:'/projects',
-            element: <Projects />,
-        },
-        {
-            path:'/projects/:slug_az',
-            element: <ProjectSingle />,
-        },
-        {
-            path:'/certificats',
-            element: <Certificats />,
-        },
-        {
-            path:'/contact',
-            element: <Contact />,
-        },
-    ]
-    const router =useRoutes(minirouter)
-    return <>{router}</>
-}
 
-export default Router
+  const minirouter = [
+    {
+      path: "/",
+      element: <Home data={data} />,
+      exact: true,
+    },
+    {
+      path: "/about",
+      element: <About />,
+    },
+    {
+      path: "/services",
+      element: <Services />,
+    },
+    {
+      path: "/projects",
+      element: <Projects />,
+    },
+    {
+      path: "/projects/:slug_az",
+      element: <ProjectSingle />,
+    },
+    {
+      path: "/certificats",
+      element: <Certificats />,
+    },
+    {
+      path: "/contact",
+      element: <Contact />,
+    },
+  ];
+  const router = useRoutes(minirouter);
+  return <>{router}</>;
+};
+
+export default Router;
